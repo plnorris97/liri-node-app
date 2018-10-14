1632f86ee9f387ab120d281f269bee570a99bfb2
Tricia Norris <tricia@Tricias-MacBook-Air.local>
2018-10-14 13:16:27 -0400

    updating files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# liri-node-app
+#liri-node-app
+
+##Summary
+
+##Instructions
+1. Open your computer's terminal.
+1. Navigate to the folder where the application **liri.js** is stored.
+1. From the command line prompt you can ask LIRI to fetch you information on movies, music and bands.
+1. The commands you should use are as follows.
+    -Search for a band's concert: `concert-this`. It will render the name of the venue, venue location and date of the concert.
+    -Search for a song: `spotify-this-song`. It will render the artist(s), song name, preview link of the song from Spotify, album the song is from. If you do not provide a song, you will receive results for "The Sign" by Ace of Base.
+    -Search for a movie: `movie-this`. It will render the title of the movie, year the movie came out, IMDB rating, Rotten Tomatoes rating, country where it was produced, language of the movie, plot and actors. If you do not provide a movie title, you will receive results for the movie "Mr. Nobody".
+    -`do-what-it-says`
+
+
+
--- liri.js
+++ liri.js
@@ -1,54 +1,192 @@
-```js
 require("dotenv").config();
-```
 
 // ========================================================================================================================
 
 var keys = require("./keys.js");
 
-// Include the Node Spotify and Request NPM packages for OMDB and Bands in Town
+// Include the node packages
 var request = require("request");
-var spotify = require("node-spotify-api");
-var bands = require("request");
 
-var omdb = new Omdb(keys.Omdb);
-
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
-
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-// Then run a request to the Spotify API with the song specified
-request("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+var fs = require("fs");
 
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
+// var band = require("request");
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+var moment = require("moment");
+moment().format();
+
+var band = new BandsinTown(keys.bands);
+
+
+// Grab search command line argument to find a concert for a band
+var search = process.argv[2];
+// Joining the remaining arguments since a band name may contain spaces
+var term = process.argv.slice(3).join(" ");
+console.log(band);
+
+// By default, if no search type is provided, search for a band
+if (!search) {
+  search = "band";
 }
-});
 
-var bands = new BandsinTown(keys.bands);
-
-// Then run a request to the Bands in Town API with the band name specified
-request("http://www.omdbapi.com/?t=" + band + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+// By default, if no search term is provided, search for "Carbon Leaf"
+if (!term) {
+  term = "Carbon Leaf";
 }
-});
+
+// Print whether searching for a movie, print the term as well
+if (search === "band") {
+  console.log("Searching for band");
+  tv.findShow(term);
+  
+} 
+
+// Create the band constructor
+var band = function() {
+  // divider will be used as a spacer between the tv data we print in log.txt
+  var divider = "\n------------------------------------------------------------\n\n";
+
+    // findShow takes in the name of a tv show and searches the tvmaze API
+    this.findShow = function(band) {
+      var URL = "https://rest.bandsintown.com/artists/" + band;
+  
+      request(URL, function(err, response, body) {
+      // Parse the response body (string) to a JSON object
+      var jsonData = JSON.parse(body)[0];
+  
+      // showData ends up being the string containing the show data we will print to the console
+      // Name of the venue
+      // Venue location
+      // Date of the Event (use moment to format this as "MM/DD/YYYY") 
+
+      var showData = [
+          "Name: " + jsonData.name,
+          "Venue: " + jsonData.venue.name,
+          "Date: " + jsonData.datetime,
+      ].join("\n\n");
+  
+      // Append showData and the divider to log.txt, print showData to the console
+      fs.appendFile("log.txt", showData + divider, function(err) {
+          if (err) throw err;
+          console.log(showData);
+      });
+      });
+  };
+};
+
+
+
+// Grab search command line argument to find an artist's song
+var search = process.argv[2];
+// Joining the remaining arguments since an artist name may contain spaces
+var term = process.argv.slice(3).join(" ");
+console.log(artist);
+
+// By default, if no search type is provided, search for a song
+if (!search) {
+  search = "song";
+}
+
+// By default, if no search term is provided, search for "The Sign"
+if (!term) {
+  term = "The Sign";
+}
+
+// Print whether searching for a song, print the song name as well
+if (search === "song") {
+  console.log("Searching for song");
+  tv.findSong(term);
+  
+} 
+// Create the music constructor
+var artist = function() {
+    // divider will be used as a spacer between the data we print in log.txt
+    var divider = "\n------------------------------------------------------------\n\n";
+    
+    //  takes in the name of a song and searches the spotify API
+    this.findSong = function(song) {
+        var URL = "" + song;
+    
+        request(URL, function(err, response, body) {
+        // Parse the response body (string) to a JSON object
+        var jsonData = JSON.parse(body);
+    
+        // showData ends up being the string containing the show data we will print to the console
+        var artistData = [
+            "Show: " + jsonData.name,
+            "Genre(s): " + jsonData.genres.join(", "),
+            "Rating: " + jsonData.rating.average,
+            "Network: " + jsonData.network.name,
+            "Summary: " + jsonData.summary
+        ].join("\n\n");
+    
+        // Append showData and the divider to log.txt, print showData to the console
+        fs.appendFile("log.txt", artistData + divider, function(err) {
+            if (err) throw err;
+            console.log(artistData);
+        });
+        });
+    }; 
+};
+
+
+var omdb = keys.omdb;
+
+// Grab search command line argument to search for a movie
+var search = process.argv[2];
+// Joining the remaining arguments since movie title may contain spaces
+var term = process.argv.slice(3).join(" ");
+
+// By default, if no search type is provided, search for a movie
+if (!search) {
+  search = "movie";
+}
+
+// By default, if no search term is provided, search for "Mr. Nobody"
+if (!term) {
+  term = "mr. nobody";
+}
+
+// Print whether searching for a movie, print the term as well
+if (search === "movie") {
+  console.log("Searching for movie");
+  tv.findMovie(movie);
+  
+} 
+
+// Create the movie constructor
+var movie = function() {
+    // divider will be used as a spacer between the tv data we print in log.txt
+    var divider = "\n------------------------------------------------------------\n\n";
+  
+    // findShow takes in the name of a tv show and searches the tvmaze API
+    this.findMovie = function(movie) {
+      var URL = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + omdb;
+  
+      request(URL, function(err, response, body) {
+        // Parse the response body (string) to a JSON object
+        var jsonData = JSON.parse(body);
+  
+        // showData ends up being the string containing the show data we will print to the console
+        var movieData = [
+          "Title: " + jsonData.title,
+          "Year: " + jsonData.year,
+          "IMDB Rating: " + jsonData.search.imdbRating,
+          "Rotten Tomatoes Rating: " + jsonData.ratings[1],
+          "Country: " + jsonData.search.country,
+          "Language: " + jsonData.search.language,
+          "Plot: " + jsonData.search.plot,
+          "Actors: " + jsonData.search.actors
+        ].join("\n\n");
+  
+        // Append showData and the divider to log.txt, print showData to the console
+        fs.appendFile("log.txt", movieData + divider, function(err) {
+          if (err) throw err;
+          console.log(movieData);
+        });
+      });
+    };
+};
+
